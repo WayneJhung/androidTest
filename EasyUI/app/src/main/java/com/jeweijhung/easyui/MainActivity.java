0a0d1e59d4330875966aae6eb1b8ff80f2cf2eec
Je-Wei Jhung <jhungjewei@gmail.com>
2015-08-31 19:19:52 +0800

    a little change

--- EasyUI/app/src/main/java/com/jeweijhung/easyui/MainActivity.java
+++ EasyUI/app/src/main/java/com/jeweijhung/easyui/MainActivity.java
@@ -1,16 +1,79 @@
 package com.jeweijhung.easyui;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity; //支援舊的手機使用lib
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputText;
+    private static Toast toast;
+    private CheckBox hide;
+    private SharedPreferences sp; //讀
+    private SharedPreferences.Editor editor; //寫
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sp = getSharedPreferences("memo", Context.MODE_PRIVATE);
+        editor = sp.edit();
+
+        inputText = (EditText) findViewById(R.id.editText);
+        inputText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                editor.putString("input", inputText.getText().toString());
+                editor.commit();
+
+                if (keyCode == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN) {
+                    magic(v);
+                    return true;
+                }
+                return false;
+            }
+        });
+        inputText.setText(sp.getString("input", ""));
+
+        hide = (CheckBox) findViewById(R.id.checkBox);
+        hide.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean("hide", isChecked);
+                editor.commit();
+            }
+        });
+        hide.setChecked(sp.getBoolean("hide", false));
+    }
+
+    public void magic(View view) {
+        String text = inputText.getText().toString();
+        inputText.setText("");
+
+        if (hide.isChecked()) {
+            text = text.replace("a", "*");
+        }
+        makeTextAndShow(this, text, Toast.LENGTH_LONG);
+    }
+
+
+    private static void makeTextAndShow(final Context context, final String text, final int duration) {
+        if (toast == null) {
+            toast = Toast.makeText(context, text, duration);
+        } else {
+            toast.setText(text);
+            toast.setDuration(duration);
+        }
+        toast.show();
     }
 
     @Override
